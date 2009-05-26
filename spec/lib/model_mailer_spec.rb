a24d13778e73699d4dcca9f0a11beaad44475f2b
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-05-26 14:29:32 -0400

    cleanup spec-fs

--- spec/lib/model_mailer_spec.rb
+++ spec/lib/model_mailer_spec.rb
@@ -12,7 +12,7 @@ describe ModelMailer do
   
   
   
-  context "saving the model" do
+  context "when saving the watched model" do
     before(:each) do
       @expected_recipient = expected_recipient = "corey@example.com"
       ModelMailer.configure do
@@ -22,13 +22,19 @@ describe ModelMailer do
       @contact = ContactForm.new
     end
     
-    it "receives the model through the after_create hook" do
+    it "receives the newly-created model through the after_create hook" do
       ModelMailer.should_receive(:after_create).with(@contact)
+      @contact.save
+    end
+    it "calls deliver_notification_email" do
+      ModelMailer.should_receive(:deliver_notification_email).with(anything, @contact)
       @contact.save
     end
   end
 
-  context "delivering the mail" do
+  context "when email is being created and delivered" do
+    include EmailSpec::Helpers
+    include EmailSpec::Matchers
     before(:each) do
       @expected_recipient = expected_recipient = "corey@example.com"
       ModelMailer.configure do
@@ -37,31 +43,21 @@ describe ModelMailer do
       end
       @contact = ContactForm.new
     end
-    it "delivers an email" do
-      ModelMailer.should_receive(:deliver_notification_email).with(anything, @contact)
-      ModelMailer.after_create @contact
+    before(:each) do
+      @configuration = mock("configuration", :deliver_to => @expected_recipient)
+      @email = ModelMailer.deliver_notification_email(@configuration, @contact)
+    end
+
+    it "delivers to the recipient on the configuration" do
+      @email.should deliver_to(@configuration.deliver_to)
     end
     
-    context "setting parameters on mail" do
-      include EmailSpec::Helpers
-      include EmailSpec::Matchers
-
-      before(:each) do
-        @configuration = mock("configuration", :deliver_to => @expected_recipient)
-        @email = ModelMailer.deliver_notification_email(@configuration, @contact)
-      end
-
-      it "delivers to the recipient on the configuration" do
-        @email.should deliver_to(@configuration.deliver_to)
-      end
-      
-      it "passes the contact object into the mail" do
-        @email.should have_text(/#{@contact}/)
-      end
+    it "passes the contact object into the mail" do
+      @email.should have_text(/#{@contact}/)
     end
   end
 
-  context "rendering email according to model name" do
+  context "when rendering email according to model name" do
     include EmailSpec::Helpers
     include EmailSpec::Matchers
     before(:each) do
@@ -76,12 +72,12 @@ describe ModelMailer do
       end
       @configuration = mock("configuration", :deliver_to => @expected_recipient)
     end
-    it "renders contact_form_delivery_notification for ContactForm" do
+    it "renders contact_form/notification_email.rb for ContactForm" do
       contact = ContactForm.new
       @email = ModelMailer.deliver_notification_email(@configuration, contact)
       @email.should have_text(/Contact Email/)
     end
-    it "renders foo_delivery_notification for Foo" do
+    it "renders foo/notification_email.rb for Foo" do
       foo = Foo.new
       @email = ModelMailer.deliver_notification_email(@configuration, foo)
       @email.should have_text(/Foo Template/)
