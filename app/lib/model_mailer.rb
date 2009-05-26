1abe904a52e2ba8eb136f73a078fa0fa44342e8b
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-05-26 11:54:50 -0400

    specs passing for configuring/observing model

--- app/lib/model_mailer.rb
+++ app/lib/model_mailer.rb
@@ -1,14 +1,47 @@
 class ModelMailer
   def self.configure(&block)
-    configuration = Configuration.new
-    configuration.instance_eval &block
+    @configurations ||= {}
+    configuration = configuration_from &block
+    configure_model configuration.watched_model
+    save_the configuration
+  end  
+  
+  def self.after_create(model)
+    model_name = model.class.name.underscore.to_sym
+    configuration = @configurations[model_name]
+    recipients configuration.deliver_to
   end
   
+  private
+  def self.save_the(configuration)
+    @configurations[configuration.watched_model] = configuration
+  end
+  def self.configure_model(model_name)
+    return if @configurations.has_key?(model_name)
+    observe_model model_name
+  end
+  
+  def self.configuration_from(&block)
+    configuration = Configuration.new
+    configuration.instance_eval &block
+    configuration
+  end
+  
+  def self.observe_model(model_name)
+    model = model_name.to_s.camelize.constantize
+    model.after_create ModelMailer
+  end
+  
+  
   class Configuration
+    attr_reader :deliver_to
+    attr_reader :watched_model
+    
     def model(model_name)
-      Foo.after_create ModelMailer
+      @watched_model = model_name
     end
     def recipients(recipient)
+      @deliver_to = recipient
     end
   end
 end
--- spec/lib/model_mailer_spec.rb
+++ spec/lib/model_mailer_spec.rb
@@ -1,22 +1,46 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ModelMailer do
-  context "configuring" do
-    it "can be configured" do
-      ModelMailer.configure do
-      end
-    end
-    it "configures the model" do
-      Foo.should_receive(:after_create).with(ModelMailer)
+  context "when configuring" do
+    it "can be configured with model and recipients" do
       ModelMailer.configure do
         model :foo
-      end
-    end
-    
-    it "has a recipients configuration" do
-      ModelMailer.configure do
         recipients "corey@example.com"
       end
     end
   end
+  
+  
+  
+  context "saving the model" do
+    before(:each) do
+      @expected_recipient = expected_recipient = "corey@example.com"
+      ModelMailer.configure do
+        model :contact_form
+        recipients expected_recipient
+      end
+      @contact = ContactForm.new
+    end
+    
+    it "receives the model through the after_create hook" do
+      ModelMailer.should_receive(:after_create).with(@contact)
+      @contact.save
+    end
+  end
+
+  context "delivering the mail" do
+    before(:each) do
+      @expected_recipient = expected_recipient = "corey@example.com"
+      ModelMailer.configure do
+        model :contact_form
+        recipients expected_recipient
+      end
+      @contact = ContactForm.new
+    end
+    it "sets the recipient" do
+      ModelMailer.should_receive(:recipients).with(@expected_recipient)
+      ModelMailer.after_create @contact
+    end
+  end
+  
 end
